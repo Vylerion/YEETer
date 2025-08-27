b3d45b88a3be5492c1cc9133ea01eaa7216f3683
King Harkeenhuart <harkeenhuart@gmail.com>
2025-08-27 14:02:35 +0000

    make the final position wayyyyyyyyyy further in

--- src/components/three-canvas.tsx
+++ src/components/three-canvas.tsx
@@ -24,7 +24,7 @@ const ThreeCanvas: React.FC = () => {
     // Camera
     const camera = new THREE.PerspectiveCamera(75, currentMount.clientWidth / currentMount.clientHeight, 0.1, 1000);
     const startPosition = new THREE.Vector3(0, 35, 35);
-    const endPosition = new THREE.Vector3(0, 8, 18);
+    const endPosition = new THREE.Vector3(0, 4, 8);
     camera.position.copy(startPosition);
 
     // Renderer
@@ -344,4 +344,5 @@ const ThreeCanvas: React.FC = () => {
 
 export default ThreeCanvas;
 
+    
     
